ce008f172fe3a6b171ac2240631dace3fc5d4ee8
Rukin Danil <rukin2018d@gmail.com>
2023-03-15 19:29:23 +0700

    add api

---
+++ PasswordGeneratorCICD.Api/Controllers/PasswordGeneratorController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PasswordGeneratorCICD.Application.Dtos;
+using PasswordGeneratorCICD.Application.PasswordGenerator.Commands;
+using PasswordGeneratorCICD.SharedKernel.Results;
+
+namespace PasswordGeneratorCICD.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordGeneratorController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public PasswordGeneratorController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<string>> CreatePassword([FromBody] PasswordOptionsDto options)
+        {
+            CreatePasswordCommand command = new(options);
+            Result<string> result = await _mediator.Send(command);
+            if (result.IsSuccess)
+                return Ok(result.Value);
+            else
+                return BadRequest(result.Errors);
+        }
+    }
+}
